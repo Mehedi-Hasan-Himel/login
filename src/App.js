a2013ed359847fbbffd0c77fde3b3471af9e0935
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-10-15 02:26:48 +0600

    github log in

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
 import "./App.css";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+  getAuth,
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 import app from "./firebase.init";
+import { useState } from "react";
 
 const auth = getAuth(app);
 
 function App() {
-  const provider = new GoogleAuthProvider();
+  const [user, setUser] = useState({});
+  const googleProvider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
+
   const handleGoogleSignIn = () => {
-    signInWithPopup(auth, provider)
-      .then(result => {
-        const user = result.user
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
         console.log(user);
       })
-      .catch(error => {
-      console.error(`error`,error);
-    })
+      .catch((error) => {
+        console.error(`error`, error);
+      });
+  };
 
-  }
+  const handleGithubSignIn = () => {
+    signInWithPopup(auth, githubProvider)
+      .then((result) => {
+        const user = result.user;
+        setUser(user)
+        console.log(user);
+      })
+
+      .catch((error) => {
+        console.error(`error`, error);
+      });
+  };
+  
+  
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({});
+      })
+      .catch((error) => {
+        setUser({});
+      });
+  };
 
   return (
     <div className="App">
-      <button onClick={handleGoogleSignIn}>Google Sign In</button>
+      {user.uid ? (
+        <button onClick={handleSignOut}>Sign Out</button>
+      ) : (
+        <>
+          <button onClick={handleGoogleSignIn}>Google Sign In</button>
+          <button onClick={handleGithubSignIn}>Github Sign In</button>
+        </>
+      )}
+      <h2>Name: {user.displayName}</h2>
+      <p>I know your email address: {user.email}</p>
+      <img src={user.photoURL} alt="" />
     </div>
   );
 }
